c2172ecbca14cb12b803b6a5f9096e482b3ddbd1
mohamed-bkhaled <bkhaled.mohamed171@gmail.com>
2023-09-13 10:28:34 +0200

    todo app v1.1

--- todo-frontend/src/App.jsx
+++ todo-frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Todos from "./components/Todos";
 import TodoInput from "./components/todoInput";
 import { v4 as uuidv4 } from "uuid";
@@ -6,15 +6,34 @@ import { v4 as uuidv4 } from "uuid";
 uuidv4();
 
 function App() {
-  const [todos, setTodos] = useState([{ task: "eat lanuch" }]);
+  const [todos, setTodos] = useState(() => {
+    const storedTodos = localStorage.getItem("todos");
+    return storedTodos
+      ? JSON.parse(storedTodos)
+      : [{ id: uuidv4(), task: "pet dog" }];
+  });
 
   const addTodo = (todo) => {
     setTodos([...todos, { id: uuidv4(), task: todo, completed: false }]);
   };
+  const deleteTodo = (id) => {
+    const updatedTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(updatedTodos);
+  };
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  useEffect(() => {
+    const storedTodos = localStorage.getItem("todos");
+    if (storedTodos) {
+      setTodos(JSON.parse(storedTodos));
+    }
+  }, []);
 
   return (
     <div className="w-full h-screen flex justify-center items-center">
-      <div className="w-5/12 min-h-1/2 bg-slate-800 p-20 rounded-lg">
+      <div className="xl:w-5/12 md:w-3/4 w-11/12 min-h-1/2 bg-slate-800 p-3 md:p-20 rounded-lg">
         <h1 className="text-white text-center text-2xl font-bold pb-12">
           Things To Get Done!
         </h1>
@@ -22,9 +41,13 @@ function App() {
         <TodoInput addTodo={addTodo} />
         <h2 className="text-white pt-3">Todos:</h2>
         {todos.map((todo, index) => (
-          <Todos task={todo} key={index} />
+          <Todos task={todo} key={index} deleteTodo={deleteTodo} />
         ))}
+        <p className=" text-xs text-gray-400 text-center">
+          note: click on the todo to mark it done.
+        </p>
       </div>
+      <div></div>
     </div>
   );
 }
--- todo-frontend/src/components/Todos.jsx
+++ todo-frontend/src/components/Todos.jsx
@@ -1,14 +1,33 @@
 /* eslint-disable react/prop-types */
+
+import { useState } from "react";
+
 // eslint-disable-next-line react/prop-types
-function Todos({ task }) {
+function Todos({ task, deleteTodo }) {
+  const [clicked, setclicked] = useState(false);
+
+  const handleDelete = () => {
+    deleteTodo(task.id);
+  };
+
   return (
     <div className="flex flex-row  flex-wrap justify-between px-3 py-2 items-center rounded-md bg-slate-500 mb-2">
-      <p className="text-white py-2 flex-[3] min-w-fit">{task.task}</p>
+      <p
+        onClick={() => setclicked(!clicked)}
+        className={`text-white py-2 flex-[3] cursor-pointer min-w-fit ${
+          clicked ? "line-through text-gray-800" : ""
+        }`}
+      >
+        {task.task}
+      </p>
       <div className="flex flex-row gap-2  justify-end">
-        <button className=" bg-green-300 py-1 px-3 rounded hover:bg-green-400">
+        {/* <button className=" bg-green-300 py-1 px-3 rounded hover:bg-green-400">
           edit
-        </button>
-        <button className="bg-red-300 py-1 px-3 rounded  hover:bg-red-400">
+        </button> */}
+        <button
+          onClick={handleDelete}
+          className="bg-red-300 py-1 px-3 rounded  hover:bg-red-400"
+        >
           delete
         </button>
       </div>
